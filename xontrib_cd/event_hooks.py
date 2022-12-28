1cba542c1ed7ca598ea92657524ca08998121dd2
eugenesvk <eugenesvk@users.noreply.github.com>
2022-12-28 23:34:35 +0700

    move split cd into the new template format

--- xontrib_cd/event_hooks.py
+++ xontrib_cd/event_hooks.py
@@ -1,9 +1,33 @@
-def activate_env(path:str):
-    # lazy import to reduce startup cost
-    from xontrib.voxapi import Vox
+from builtins import __xonsh__  # XonshSession (${...} is '__xonsh__.env')
+envx = __xonsh__.env
 
-    vox = Vox()
-    vox.activate(path)
+_alt_symlink_flag      = envx.get('XONTRIB_CD_ALTSYMLINKFLAG' , False)
+_alt_symlink_func      = envx.get('XONTRIB_CD_ALTSYMLINKFUNC' , False)
+_symlink_always_follow = envx.get('XONTRIB_CD_SYMLINKAlWAYSON', False)
 
-def listen_cd(olddir, newdir, **_):
-    activate_env(newdir)
+def listen_cmd_macro(        cmd, **kw):
+  if cmd.startswith('cd -P '):
+    return 'cd -P! ' + cmd[6:]
+  elif (_alt_symlink_flag    and                 # alt flags for following symlinks
+  (cmd.startswith('cd -p ')  or
+   cmd.startswith('cd -f ')  or
+   cmd.startswith('cd -s ') )):
+    return   'cd -P! ' + cmd[6:]
+  elif\
+  (cmd.startswith('cd '    ) and not
+   cmd.startswith('cd -P! ')):                   # exclude previous matches
+    if _symlink_always_follow:
+      return 'cd -P! ' + cmd[3:]
+    else:
+      return 'cd! '    + cmd[3:]
+  else:
+    return cmd
+
+def listen_cmd_macro_symlink(cmd, **kw):         # alt functions for following symlinks
+  if (_alt_symlink_func      and                 # duplicate check to main
+  (cmd.startswith('cdp ')    or
+   cmd.startswith('cdf ')    or
+   cmd.startswith('cds ') )):
+    return   'cd -P! ' + cmd[4:]
+  else:
+    return cmd
